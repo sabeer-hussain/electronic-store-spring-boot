f3a96f8e042536a46672d9d391963cbd3940f9b3
sabeer-hussain <78043020+sabeer-hussain@users.noreply.github.com>
2023-09-01 16:35:57 +0530

    User Module Implementation (#3) (#4)
    * Project initial setup (#1)
    
    * User Module Implementation (#3)

---
+++ src/main/java/com/sabeer/electronic/store/services/UserService.java
@@ -0,0 +1,32 @@
+package com.sabeer.electronic.store.services;
+
+import com.sabeer.electronic.store.dtos.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+
+    // create user
+    UserDto createUser(UserDto userDto);
+
+    // update user
+    UserDto updateUser(UserDto userDto, String userId);
+
+    // delete user
+    void deleteUser(String userId);
+
+    // get all users
+    List<UserDto> getAllUsers();
+
+    // get single user by id
+    UserDto getUserById(String userId);
+
+    // get single user by email
+    UserDto getUserByEmail(String email);
+
+    // search user
+    List<UserDto> searchUser(String keyword);
+
+    // other user specific features
+
+}
