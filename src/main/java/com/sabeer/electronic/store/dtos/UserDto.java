e440941b4783b8fa7b3126bad3ba155a50119e02
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-01 19:16:41 +0530

    API Validation changes

--- src/main/java/com/sabeer/electronic/store/dtos/UserDto.java
+++ src/main/java/com/sabeer/electronic/store/dtos/UserDto.java
@@ -1,6 +1,12 @@
 package com.sabeer.electronic.store.dtos;
 
+import com.sabeer.electronic.store.validator.ImageNameValid;
 import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -9,10 +15,25 @@ import lombok.*;
 @Builder
 public class UserDto {
     private String userId;
+
+    @Size(min = 3, max = 20, message = "Invalid Name !!")
     private String name;
+
+//    @Email(message = "Invalid User Email !!")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$", message = "Invalid User Email !!")
+    @NotBlank(message = "Email is required !!")
     private String email;
+
+    @NotBlank(message = "Password is required !!")
     private String password;
+
+    @Size(min = 4, max = 6, message = "Invalid gender !!")
     private String gender;
+
+    @NotBlank(message = "Write something about yourself !!")
     private String about;
+
+//    Custom validator
+    @ImageNameValid
     private String imageName;
 }
