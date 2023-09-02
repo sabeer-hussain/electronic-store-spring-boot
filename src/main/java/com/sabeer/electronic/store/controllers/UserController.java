794ae47350942f052e88987232e791907a5506fc
sabeer-hussain <78043020+sabeer-hussain@users.noreply.github.com>
2023-09-02 22:25:32 +0530

    Es 8/remaining features in user module (#7)
    * Create User Upload image api
    
    * Serve user image api
    
    * Delete user image when delete user

--- src/main/java/com/sabeer/electronic/store/controllers/UserController.java
+++ src/main/java/com/sabeer/electronic/store/controllers/UserController.java
@@ -1,23 +1,41 @@
 package com.sabeer.electronic.store.controllers;
 
 import com.sabeer.electronic.store.dtos.ApiResponseMessage;
+import com.sabeer.electronic.store.dtos.ImageResponse;
 import com.sabeer.electronic.store.dtos.PageableResponse;
 import com.sabeer.electronic.store.dtos.UserDto;
+import com.sabeer.electronic.store.services.FileService;
 import com.sabeer.electronic.store.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${user.profile.image.path}")
+    private String imageUploadPath;
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private FileService fileService;
 
     // create
     @PostMapping
@@ -76,4 +94,34 @@ public class UserController {
         return new ResponseEntity<>(userService.searchUser(keywords), HttpStatus.OK);
     }
 
+    // upload user image
+    @PostMapping("/image/{userId}")
+    public ResponseEntity<ImageResponse> uploadUserImage(@RequestParam("userImage") MultipartFile image, @PathVariable String userId) throws IOException {
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+
+        UserDto user = userService.getUserById(userId);
+        user.setImageName(imageName);
+        userService.updateUser(user, userId);
+
+        ImageResponse imageResponse = ImageResponse.builder()
+                                            .imageName(imageName)
+                                            .message("Image is uploaded successfully")
+                                            .success(true)
+                                            .status(HttpStatus.CREATED)
+                                        .build();
+        return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
+    }
+
+    // serve user image
+    @GetMapping("/image/{userId}")
+    public void serveUserImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+        UserDto user = userService.getUserById(userId);
+        LOGGER.info("User image name : {}", user.getImageName());
+
+        InputStream resource = fileService.getResource(imageUploadPath, user.getImageName());
+        String extension = user.getImageName().substring(user.getImageName().lastIndexOf(".")+1);
+        LOGGER.info("Extension : {}", extension);
+        response.setContentType("image/"+extension);
+        StreamUtils.copy(resource, response.getOutputStream());
+    }
 }
