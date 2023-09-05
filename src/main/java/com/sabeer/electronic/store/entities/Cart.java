05d43aa09c43960281d1d816061589877dcc769a
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-05 19:42:50 +0530

    Adding cart and cartItem entities and dtos

---
+++ src/main/java/com/sabeer/electronic/store/entities/Cart.java
@@ -0,0 +1,31 @@
+package com.sabeer.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    private String cartId;
+
+    private Date createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // mapping cart-items
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+}
