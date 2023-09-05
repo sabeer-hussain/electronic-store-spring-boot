5cece6e68d33a6c68b599179ec2d660760af7553
sabeer-hussain <78043020+sabeer-hussain@users.noreply.github.com>
2023-09-05 20:06:01 +0530

    Es 11/create cart module (#13)
    * Adding cart and cartItem entities and dtos
    
    * api to add item to cart
    
    * api to remove item from cart
    
    * api to clear cart
    
    * api to fetch cart of an user

---
+++ src/main/java/com/sabeer/electronic/store/entities/CartItem.java
@@ -0,0 +1,32 @@
+package com.sabeer.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartItemId;
+
+    // Mapping product to manage product
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    private int totalPrice;
+
+    // Mapping cart to manage cart
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
