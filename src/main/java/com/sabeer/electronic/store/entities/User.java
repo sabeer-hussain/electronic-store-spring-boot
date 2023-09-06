2a329fbe34193cc6f642b3615b235aa7899560d2
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-06 18:01:17 +0530

    Creating Order and OrderItem entities

--- src/main/java/com/sabeer/electronic/store/entities/User.java
+++ src/main/java/com/sabeer/electronic/store/entities/User.java
@@ -3,6 +3,8 @@ package com.sabeer.electronic.store.entities;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -33,4 +35,7 @@ public class User {
 
     @Column(name = "user_image_name")
     private String imageName;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Order> orders = new ArrayList<>();
 }
