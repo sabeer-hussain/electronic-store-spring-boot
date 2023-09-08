7e12e2fb9258e15e5f5adbbb872509f9780d8579
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-08 17:38:34 +0530

    Form based login authentication customization

--- src/main/java/com/sabeer/electronic/store/config/SecurityConfig.java
+++ src/main/java/com/sabeer/electronic/store/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -39,6 +41,25 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(normalUser, adminUser);
     }
      */
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Form based login authentication
+        http
+           .authorizeRequests()
+           .anyRequest()
+           .authenticated()
+           .and()
+           .formLogin()
+           .loginPage("login.html")
+           .loginProcessingUrl("/process-url")
+           .defaultSuccessUrl("/dashboard")
+           .failureUrl("error")
+           .and()
+           .logout()
+           .logoutUrl("/logout");
+        return http.build();
+    }
 
     // Database Authentication
     @Bean
