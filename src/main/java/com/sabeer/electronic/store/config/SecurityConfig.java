95bc6dcaea9079104ee61b4b7baebeba263c6abd
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-11 18:29:25 +0530

    Implement swagger to generate api docs

--- src/main/java/com/sabeer/electronic/store/config/SecurityConfig.java
+++ src/main/java/com/sabeer/electronic/store/config/SecurityConfig.java
@@ -34,6 +34,13 @@ import java.util.Arrays;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    private final String[] PUBLIC_URLS = {
+        "/swagger-ui/**",
+        "/webjars/**",
+        "/swagger-resources/**",
+        "/v3/api-docs"
+    };
+
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -115,6 +122,8 @@ public class SecurityConfig {
             .antMatchers(HttpMethod.POST, "/users")
             .permitAll()
             .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+            .antMatchers(PUBLIC_URLS)
+            .permitAll()
             .anyRequest()
             .authenticated()
             .and()
