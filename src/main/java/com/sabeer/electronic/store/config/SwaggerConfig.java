2eee0323268164b827c643f3c8a7b7e22e05233f
sabeer-hussain <msabeerhussain.edu@gmail.com>
2023-09-11 22:45:22 +0530

    Implement Spring Security in Swagger

--- src/main/java/com/sabeer/electronic/store/config/SwaggerConfig.java
+++ src/main/java/com/sabeer/electronic/store/config/SwaggerConfig.java
@@ -2,12 +2,17 @@ package com.sabeer.electronic.store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,7 +21,32 @@ public class SwaggerConfig {
     public Docket docket() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2);
         docket.apiInfo(getApiInfo());
-        return docket;
+
+        docket.securityContexts(Arrays.asList(getSecurityContext()));
+        docket.securitySchemes(Arrays.asList(getSecuritySchemes()));
+
+        ApiSelectorBuilder select = docket.select();
+        select.apis(RequestHandlerSelectors.any());
+        select.paths(PathSelectors.any());
+        Docket build = select.build();
+
+        return build;
+    }
+
+    private SecurityContext getSecurityContext() {
+        SecurityContext context = SecurityContext.builder()
+                .securityReferences(getSecurityReferences())
+                .build();
+        return context;
+    }
+
+    private List<SecurityReference> getSecurityReferences() {
+        AuthorizationScope[] scopes = { new AuthorizationScope("Global", "Access Every Thing")};
+        return Arrays.asList(new SecurityReference("JWT", scopes));
+    }
+
+    private ApiKey getSecuritySchemes() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 
     private ApiInfo getApiInfo() {
