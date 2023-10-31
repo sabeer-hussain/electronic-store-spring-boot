8108476d7e9ffa366b53b95d3b6e68e6462a3d8d
sabeer-hussain <78043020+sabeer-hussain@users.noreply.github.com>
2023-11-01 00:38:57 +0530

    ES-36/Update Search User api with pagination and sorting (#33) (#34)
    * Project initial setup (#1)
    
    * User Module Implementation (#3)
    
    * Miscellaneous Project Topics (#5)
    
    * Es 8/remaining features in user module (#7)
    
    * Es 9/create categories module (#9)
    
    * Es 10/create product module (#11)
    
    * Es 11/create cart module (#13)
    
    * Es 12/create order module (#15)
    
    * Es 13/secure apis using spring security (#17)
    
    * Es 14/create api documentation using swagger (#19)
    
    * Es 15/implement unit testing (#21)
    
    * Es 17/implement pending unit testing (#23)
    
    * Es 16/Dockerize Electronic Store backend - spring boot (#25)
    
    * ES 24/Resolving password update problem (#27)
    
    * Es 27/implement update product image api (#29)
    
    * Add User in Order in order api response dto in backend to get user information with order (#31)
    
    * ES-36/Update Search User api with pagination and sorting (#33)

--- src/main/java/com/sabeer/electronic/store/repositories/UserRepository.java
+++ src/main/java/com/sabeer/electronic/store/repositories/UserRepository.java
@@ -1,14 +1,15 @@
 package com.sabeer.electronic.store.repositories;
 
 import com.sabeer.electronic.store.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByEmailAndPassword(String email, String password);
 
-    List<User> findByNameContaining(String keyword);
+    Page<User> findByNameContaining(String keyword, Pageable pageable);
 }
